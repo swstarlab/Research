a0ce385ddc39126c2475aeeeba1bdd637fe60efb
2yunsu <2yunsu@snu.ac.kr>
2022-08-23 20:05:54 +0900

    2022/08/23

--- YJ_School/inorder_traversal.py
+++ YJ_School/inorder_traversal.py
@@ -1,28 +1,33 @@
 import sys
 
-class Node:
-    def __init__(self, data, left, right):
-        self.data = data
-        self.left = left
-        self.right = right
+# class Node:
+#     def __init__(self, data, left, right):
+#         self.data = data
+#         self.left = left
+#         self.right = right
 
 def in_order(node):
-    if node.left != None:
-        in_order(tree[node.left])
-    print(node.data, end='')
-    if node.right != None:
-        in_order(tree[node.right])
+    print("node2", node_list[i])
+    print("node2", node_list[i][2])
+    if node_list[i][2] != None:
+        in_order(node_list[i][2])
+    print(node_list[i][1], end='')
+    if node_list[i][3] != None:
+        in_order(node_list[i][3])
 
-N = int(sys.stdin.readline().rstrip())
-tree = {}
+for _ in range(10):
+    N = int(input())
+    node_list = [[i+1, '', None, None] for i in range(N)]
 
-for i in range(N):
-    data, left, right = sys.stdin.readline().split()
-    if left == '.':
-        left = None
-    if right == '.':
-        right = None
-    tree[data] = Node(data, left, right)
+    for i in range(N):
+        node = input()
+        node = node.split(' ')
+        node_list[i][0] = node[0]
+        node_list[i][1] = node[1]
+        if len(node) >= 3:
+            node_list[i][2] = node[2]
+        if len(node) >= 4:
+            node_list[i][3] = node[3]
+        print(node_list[2])
 
-in_order(tree['A'])
-print()
+        in_order(node_list[N])
